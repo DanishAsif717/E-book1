5966c3229c935b769c5a47ce3494d6b2933107cb
Muhammad Danish <danishasif717@gmail.com>
2024-09-30 19:23:38 +0500

    Add templete

---
+++ bookstore/E-Project book/E-Project book/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using E_Project_book.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace E_Project_book.Controllers
+{
+	public class HomeController : Controller
+	{
+		
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+	
+
+	
+	}
+}
---
+++ bookstore/E-Project book/E-Project book/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Project_book.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
