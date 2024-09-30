95c42249d1dcf3086274bfdeb856450d18b14d00
Muhammad Danish <danishasif717@gmail.com>
2024-09-30 21:58:45 +0500

    Add templete

--- E-Book eproject/Controllers/ProductController.cs
+++ E-Book eproject/Controllers/ProductController.cs
@@ -70,6 +70,67 @@ namespace E_Book_eproject.Controllers
             return RedirectToAction("Index");
         }
 
+
+
+        public ActionResult Edit(int Id)
+        {
+            var product = db.Products.Find(Id); // Retrieve the product by Id
+
+
+            ViewBag.CatId = new SelectList(db.Categories, "Id", "Name", product.CatId); // Set selected category
+
+            // Initially pass subcategories for the selected category
+            var subcategories = db.SubCategories.Where(s => s.CatId == product.CatId).ToList();
+            ViewBag.SubId = new SelectList(subcategories, "Id", "Name", product.SubId); // Set selected subcategory
+
+            return View(product);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product pro, IFormFile Image , string OldImage)
+        {
+            try
+            {
+                string imagename = DateTime.Now.ToString("yymmddhhmmss");
+                imagename += "-" + Path.GetFileName(Image.FileName);
+
+                var imagepath = Path.Combine(HttpContext.Request.PathBase.Value, "wwwroot/Uploads");
+                var imageValue = Path.Combine(imagepath, imagename);
+
+                using (var stream = new FileStream(imageValue, FileMode.Create))
+                {
+                    Image.CopyTo(stream);
+                }
+
+                var dbimage = Path.Combine("/Uploads", imagename);
+
+                pro.Image = dbimage;
+
+                db.Products.Add(pro);
+                db.SaveChanges();
+
+                TempData["SuccessMessage"] = "Product successfully inserted!";
+                ViewBag.CatId = new SelectList(db.Categories, "Id", "Name");
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+
+                TempData["ErrorMessage"] = "Records Does not inserted!";
+
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+
         [Authorize(Roles = "User")]
         public IActionResult ProductDetails(int id)
         {
@@ -84,8 +145,41 @@ namespace E_Book_eproject.Controllers
             return View(data);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update (Product pro, IFormFile Image, string OldImage)
+        {
 
+            if (Image != null && Image.Length > 0) // Nayi image upload hui hy
+            {
+                string imagename = DateTime.Now.ToString("yymmddhhmmss") + "-" + Path.GetFileName(Image.FileName);
+                var imagepath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Uploads");
+                var imageValue = Path.Combine(imagepath, imagename);
 
+                // Save the new image
+                using (var stream = new FileStream(imageValue, FileMode.Create))
+                {
+                    Image.CopyTo(stream);
+                }
+
+                // Save image path to the database
+                var dbimage = Path.Combine("/Uploads", imagename);
+                pro.Image = dbimage;
+            }
+            else // Nayi image upload nahi hui
+            {
+                // Purani image ko hi rakhein
+                pro.Image = OldImage;
+            }
+
+            db.Entry(pro).State = EntityState.Modified; // Update product
+            db.SaveChanges();
+
+            TempData["SuccessMessage"] = "Product successfully updated!";
+            return RedirectToAction("Index");
+        }
 
     }
+
+
 }
